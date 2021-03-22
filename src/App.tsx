65228d296574d1be2cd0dc7228219932316e164f
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-22 23:57:47 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,13 @@ import { UserProfile } from "./types/userProfile";
 
 export default function App() {
   const [userProfile, setUserProfile] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const onclickFetchUser = () => {
+    setLoading(true);
+    setError(false);
+
     axios
       .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
       .then((res) => {
@@ -18,11 +24,25 @@ export default function App() {
           address: `${user.address.city}${user.address.suite}${user.address.street}`
         }));
         setUserProfile(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
       });
   };
   return (
     <div className="App">
       <button onClick={onclickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{color: "red"}}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+      )}
       {userProfile.map((user) => (
         <UserCard key={user.id} user={user} />
       ))}
