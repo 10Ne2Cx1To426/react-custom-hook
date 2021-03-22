e6b4d824f9c38e158157c0b4020958a1258d8b4e
Sena <73450328+10Ne2Cx1To426@users.noreply.github.com>
2021-03-22 23:51:15 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import "./styles.css";
 import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
 
 const user = {
   id: 1,
@@ -9,8 +13,26 @@ const user = {
 };
 
 export default function App() {
+  const [userProfile, setUserProfile] = useState<Array<UserProfile>>([]);
+  const onclickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfile(data);
+      });
+  };
   return (
     <div className="App">
+      <button onClick={onclickFetchUser}>データ取得</button>
+      {userProfile.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
       <UserCard user={user} />
     </div>
   );
